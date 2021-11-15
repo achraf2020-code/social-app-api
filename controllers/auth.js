f4a71b561588db8647ad9a5d52c0c3401b72c0a4
achraf2020-code <achraf-saidi@outlook.com>
2021-11-15 01:04:44 +0100

    add login controller

--- controllers/auth.js
+++ controllers/auth.js
@@ -8,7 +8,21 @@ require('express-async-errors')
 module.exports = {
     login:async (req,res)=>{
         const {email,password} = req.body
-        res.json({email,password})
+        if(!email || !password){
+            throw new BadRequestError('Password and Email are required! ')
+        }
+        const user =await User.findOne({email:email})
+        
+        if(!user){
+            throw new UnauthorizedRequest ('Please check your email or password! ')
+        }
+        const isMatch = await bcrypt.compare(password,user.password)
+        if(!isMatch){
+            throw new UnauthorizedRequest ('Please check your email or password! ')
+        }
+        const token = jwt.sign({id:user._id,name:user.name},process.env.JWT_SECRET,{expiresIn:process.env.JWT_LIFE_TIME})
+
+        res.status(StatusCodes.OK).json({user:{name:user.name,token}})
     },
     register : async (req,res)=>{
         const {password,email,name} = req.body
